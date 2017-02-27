b3c55bcb8b3661649ec5e93719b5e322d36f5a1a
JacobWylie <jacobwylie@gmail.com>
2017-02-27 16:33:29 +0100

    added media.js to create a prototype chain inheritance

--- app.js
+++ app.js
@@ -14,17 +14,14 @@ var playButton = document.getElementById("play");
 playButton.onclick = function() {
 	playlist.play();
 	playlist.renderInElement(playlistElement);
-
 }
 var nextButton = document.getElementById("next");
 nextButton.onclick = function() {
 	playlist.next();
 	playlist.renderInElement(playlistElement);
-
 }
 var stopButton = document.getElementById("stop");
 stopButton.onclick = function() {
 	playlist.stop();
 	playlist.renderInElement(playlistElement);
-
 }
