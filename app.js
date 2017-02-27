0fd85172ce2132612fc56d5f12ed82988958e03d
JacobWylie <jacobwylie@gmail.com>
2017-02-27 16:23:02 +0100

    Added functionality and render to element in document

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var playlist = new playlist();
+var playlist = new Playlist();
 
 var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
 var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves", "3:43");
 
 playlist.add(hereComesTheSun);
 playlist.add(walkingOnSunshine);
+
+var playlistElement = document.getElementById('playlist');
+
+playlist.renderInElement(playlistElement);
+
+var playButton = document.getElementById("play"):
+var nextButton = document.getElementById("next"):
+var stopButton = document.getElementById("stop"):
